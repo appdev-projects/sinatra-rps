425cfd0fc24f5cd7beb1c5d774ced310eeca6af7
Grace Lu <155923103+ggracelu@users.noreply.github.com>
2025-04-15 23:24:16 +0000

    finished rps

--- app.rb
+++ app.rb
@@ -2,8 +2,51 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:home)
+end
+
+get("/rock") do
+  @our_move = "rock"
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+
+  erb(:results)
+end
+
+get("/paper") do
+  @our_move = "paper"
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  
+  if @comp_move == "paper"
+    @outcome = "tied"
+  elsif @comp_move == "scissors"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:results)
+end
+
+get("/scissors") do
+  @our_move = "scissors"
+  moves = ["rock", "paper", "scissors"]
+  @comp_move = moves.sample
+  
+  if @comp_move == "scissors"
+    @outcome = "tied"
+  elsif @comp_move == "rock"
+    @outcome = "lost"
+  else
+    @outcome = "won"
+  end
+  erb(:results)
 end
